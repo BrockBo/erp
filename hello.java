9d4c008e44a819cda8a58ba90e0ffd58184cf1eb
kabo <371562867@qq.com>
2015-11-03 11:43:55 +0800

    change hello add sub

--- hello.java
+++ hello.java
@@ -1,11 +1,14 @@
 public class hello{
 	public static void main(String args[]){
-		System.out.println("Hello,World");
+		System.out.println("Hello,World!");
 	}
 	public void test(){
 		System.out.println("test");
 	}
 	public void add (){
 		System.out.println("add")
+	}
+	public void sub (){
+		System.out.println("sub")
 	}
 }
